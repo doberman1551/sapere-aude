e0693a471329ddc8c808cf727967fced71f32c20
Dobromir Kotsev <dobromir.kotsev@gmail.com>
2021-07-26 14:51:56 +0300

    substack links

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -18,7 +18,7 @@ const About = () => (
             
 
           </AboutText>
-          <Button as="a" href="https://sapereaudehq.substack.com/p/coming-soon?r=o30d6&utm_campaign=post&utm_medium=web&utm_source=copy">I want in!</Button>
+          <Button as="a" href="https://sapereaudehq.substack.com/embed">I want in!</Button>
       </AboutWrapper>
 
     </StyledContainer>
